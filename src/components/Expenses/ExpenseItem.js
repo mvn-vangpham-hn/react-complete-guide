ebb4a2d1522ab8ca4878b814a81e856a83068e20
Vang Pham T <vang.pham@monstar-lab.com>
2022-01-07 01:12:59 +0700

    s5: render list and codition

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,9 @@
 import React, { useState } from "react";
-
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-
 const ExpenseItem = (props) => {
     const [title, setTitle] = useState(props.title);
-    const changeTitle = () => {
-        setTitle("updated");
-        console.log(123, title);
-    };
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
@@ -18,9 +11,7 @@ const ExpenseItem = (props) => {
                 <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={changeTitle}>Change title</button>
         </Card>
     );
 };
-
 export default ExpenseItem;
